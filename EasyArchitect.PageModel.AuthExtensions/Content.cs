9ba738ffa4fe7ffeda396cac5ebfffcb94fc1dcf
Gelis <wergelis@msn.com>
2024-05-29 16:35:44 +0800

    加入屬於 Razor Pages 的 AuthExtensions 套件

---
+++ EasyArchitect.PageModel.AuthExtensions/Content.cs
@@ -0,0 +1,67 @@
+﻿namespace EasyArchitect.PageModel.AuthExtensions
+{
+    /// <summary>
+    /// NewCookie 的實際內容（Indexer 物件）
+    /// </summary>
+    public class Content
+    {
+        private Dictionary<string, object> _cookieValue = new Dictionary<string, object>();
+        /// <summary>
+        /// 直接取得 NewCookie 的內容
+        /// </summary>
+        public Dictionary<string, object> CookieValue
+        {
+            get
+            {
+                return _cookieValue;
+            }
+        }
+        /// <summary>
+        /// NewCookie 的 Indexer: 透過 CookieID 取得對應的 Cookie Values 內容
+        /// </summary>
+        /// <param name="key">對應在 Cookie 內容 (Values) 的索引 key 值</param>
+        /// <returns>回傳當前的 Cookie 索引內容值 (object)</returns>
+        public string this[string key]
+        {
+            get
+            {
+                object result;
+                if (_cookieValue.TryGetValue(key, out result))
+                {
+                    return result?.ToString();
+                }
+                return "";
+            }
+            set { _cookieValue[key] = value; }
+        }
+        /// <summary>
+        /// 在 NewCookie 加入新的 Values 值
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="obj"></param>
+        public void Add(string key, object obj)
+        {
+            _cookieValue.Add(key, obj);
+        }
+        /// <summary>
+        /// 取得 Doctionary 內所有的 Keys
+        /// </summary>
+        public IEnumerable<string> Keys
+        {
+            get
+            {
+                return _cookieValue.Keys.AsEnumerable();
+            }
+        }
+        /// <summary>
+        /// 取得 Dictionary 內的個數
+        /// </summary>
+        public int Count
+        {
+            get
+            {
+                return this._cookieValue.Count;
+            }
+        }
+    }
+}
---
+++ EasyArchitect.PageModel.AuthExtensions/PageFilters/CustomAuthorizationFilter.cs
@@ -0,0 +1,65 @@
+﻿using EasyArchitect.OutsideManaged.AuthExtensions.Models;
+using EasyArchitectCore.Core;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Configuration;
+using System.Security.Principal;
+using System.Text.Json;
+
+namespace EasyArchitect.PageModel.AuthExtensions.PageFilters
+{
+    /// <summary>
+    /// PageModel 的 Authorization 過濾器
+    /// 用途：用以檢查使用者是否有權限存取特定頁面
+    /// </summary>
+    public class CustomAuthorizationFilter : IAsyncPageFilter
+    {
+        private readonly IConfiguration _configuration;
+
+        public CustomAuthorizationFilter(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        /// <summary>
+        /// 回呼事件：當 PageHandler 執行時檢查身分權限
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="next"></param>
+        /// <returns></returns>
+        public async Task OnPageHandlerExecutionAsync(PageHandlerExecutingContext context, PageHandlerExecutionDelegate next)
+        {
+            bool isAuthorized = CheckUserAuthorization(context);
+            if (!isAuthorized)
+            {
+                // 如果不滿足權限的要求，可以重定向到 Index 頁面或導向錯誤頁面
+                context.HttpContext.Response.Redirect(_configuration.GetValue<string>("LoginPage"));
+            }
+
+            // 如果檢查通過，繼續執行後續的頁面處理器或過濾器
+            await next();
+        }
+        /// <summary>
+        /// 檢查身分權限
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        private bool CheckUserAuthorization(PageHandlerExecutingContext context)
+        {
+            context.HttpContext.Request.Cookies.TryGetValue(UserInfo.LOGIN_USER_INFO, out string? cookieString);
+            string? username = context.HttpContext.User.Identity!.Name;
+            if(username == null)
+            {
+                return false;
+            }
+
+            NewCookie cookie = NewCookie.GetNewCookieByString(cookieString, UserInfo.LOGIN_USER_INFO);
+            AuthenticateRequest? accSate = cookie.Values["Username"] != null ? new AuthenticateRequest() { Username = cookie.Values["Username"].ToString() } : null;
+
+            return accSate != null;    
+        }
+
+        public Task OnPageHandlerSelectionAsync(PageHandlerSelectedContext context)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
